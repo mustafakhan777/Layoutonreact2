7c4f82ce1cf144961e776b1358a32a6c011b48ef
mustafakhan777 <mustafa.khan2214@gmail.com>
2023-08-16 17:56:53 +0500

    progress

---
+++ src/App.jsx
@@ -0,0 +1,289 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div class="main">
+      <nav class="navbar navbar-expand-lg navbar-dark pt-3 sticky-top">
+          <div class="container">
+            <a class="navbar-brand fw-bold text-white" href="#">
+              DORSIN
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto ms-5 mb-2 mb-lg-0">
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link" href="#">
+                    Services
+                  </a>
+                </li>
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link">Pricing</a>
+                </li>
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link">Team</a>
+                </li>
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item mx-3 fw-semibold">
+                  <a class="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <button
+                  class="btn button1 fw-semibold btn-light text-white"
+                  type="submit"
+                >
+                  Try it Free
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+    <div id="backgroundimage1">
+      <section class="layer1">
+<div className="row hundredper">
+  <div className="col-md-3">
+  </div>
+  <div className="col-md-6">
+  <div class="onlyheight text-white d-flex flex-column justify-content-center align-items-center">
+          <div>
+            <p class="display-5 fw-semibold justifypara">
+              We help startups launch their <section className="text-center">products</section> 
+            </p>
+          </div>
+          <div>
+            <p>Etiam sed.Interdum consequat proin vestibulum class at.</p>
+          </div>
+          <div>
+            <a href="#">
+              <img
+                height="100px"
+                src="https://www.freepnglogos.com/uploads/play-button-png/red-play-button-icon-icons-15.png"
+              ></img>
+            </a>
+          </div>
+        </div>
+  </div>
+  <div className="col-md-3">
+  </div>
+</div>
+      </section>
+    </div>
+
+
+
+
+  <div class="part2 my-5">
+    <div className="row hundredper">
+      <div className="col-md-3">
+      </div>
+      <div className="col-md-6">
+        <div className="text-center text-dark fw-normal">
+          <p className="fs-1">OUR SERVICES</p>
+          <div className="border my-3 border-danger"></div>
+          <p className="text-secondary">We craft digital, graphic and dimensional thinking, to create category leading brand experiences that have meaning and add a value for our clients.
+
+</p>
+        </div>
+      </div>
+      <div className="col-md-3">
+      </div>
+    </div>
+  </div>
+
+<div className="container">
+  <div className="hundredper row">
+    <div className="col-md-4 text-center p-3">
+      <img height='50px' src="https://w7.pngwing.com/pngs/160/884/png-transparent-hatton-garden-diamond-logo-diamond-shape-gemstone-cdr-angle-thumbnail.png" alt="" />
+      <div className="mt-4">
+        <p className="fs-3">Digital Design</p>
+        <p className="text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.</p>
+      </div>
+    </div>
+    <div className="col-md-4 text-center p-3">
+      <img height='50px' src="https://cdn-icons-png.flaticon.com/512/176/176231.png" alt="" />
+      <div className="mt-4">
+        <p className="fs-3">Unlimited Colors</p>
+        <p className="text-secondary">Credibly brand standards compliant users without extensible services. Anibh euismod tincidunt ut laoreet.</p>
+      </div>
+    </div>
+    <div className="col-md-4 text-center p-3">
+      <img height='50px' src="https://w7.pngwing.com/pngs/395/567/png-transparent-computer-icons-entrepreneurship-strategy-business-strategy-people-innovation-logo-thumbnail.png" alt="" />
+      <div className="mt-4">
+        <p className="fs-3">Strategy Solutions</p>
+        <p className="text-secondary">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean necessary regelialia.</p>
+      </div>
+    </div>
+    <div className="col-md-4 text-center p-3">
+      <img height='50px' src="https://w7.pngwing.com/pngs/186/205/png-transparent-react-native-react-logos-brands-icon.png" alt="" />
+      <div className="mt-4">
+        <p className="fs-3">Awesome Support</p>
+        <p className="text-secondary">It is a paradisematic country, in which roasted parts of sentences fly into your mouth leave for the far World.
+
+</p>
+      </div>
+    </div>
+    <div className="col-md-4 text-center p-3">
+      <img height='50px' src="https://static.thenounproject.com/png/1333293-200.png" alt="" />
+      <div className="mt-4">
+        <p className="fs-3">Truly Multipurpose</p>
+        <p className="text-secondary">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+
+</p>
+      </div>
+    </div>
+    <div className="col-md-4 text-center p-3">
+      <img height='50px' src="https://png.pngtree.com/png-vector/20191021/ourmid/pngtree-vector-airplane-icon-png-image_1834580.jpg" alt="" />
+      <div className="mt-4">
+        <p className="fs-3">Easy to customize</p>
+        <p className="text-secondary">Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia.
+
+</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div className="bg-light totalheight mt-5 ">
+  <div className="container">
+  <div className="hundredper row">
+<div className="col-md-6">
+  <div className="text-start p-5">
+    <p className="fs-4 mb-3">A digital web design studio creating modern & engaging online experiences</p>
+    <p className="text-secondary my-3">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+<ul className="mt-5">
+  <li>We put a lot of effort in design.</li>
+  <li>The most important ingredient of successful website.</li>
+  <li>Sed ut perspiciatis unde omnis iste natus error sit.</li>
+  <li>Submit Your Organization.</li>
+</ul>
+  </div>
+</div>
+<div className="col-md-6">
+  <img src="http://dorsin.react.themesbrand.com/assets/images/online-world.svg"
+ alt="" height='300px' width='300px' />
+</div>
+  </div>
+  </div>
+</div>
+<div className="bg-light onlyten"></div>
+<div className="backgroundimage2">
+  <div className="layer1">
+    <div className="d-flex flex-column align-items-center justify-content-center h-100">
+      <p className="fs-1 text-white fw-normal">Build your dream website today</p>
+      <p className="text-secondary">But nothing the copy said could convince her and so it didn’t take long until a few <br /> insidious Copy Writers ambushed her.</p>
+      <button className="btn btn-light btn-rounded">View Plan and Pricing</button>
+    </div>
+  </div>
+</div>
+
+<div class="part2 my-5">
+    <div className="row hundredper">
+      <div className="col-md-3">
+      </div>
+      <div className="col-md-6">
+        <div className="text-center text-dark fw-normal">
+          <p className="fs-1">OUR PRICING</p>
+          <div className="border my-3 border-danger"></div>
+          <p className="text-secondary">Call to action pricing table is really crucial to your for your business website. Make your bids stand-out with amazing options.
+</p>
+        </div>
+      </div>
+      <div className="col-md-3">
+      </div>
+    </div>
+  </div>
+
+<div className="container">
+<div className="row hundredper">
+  <div className="col-md-4 border border-dark bordersahdow">
+    <div className="text-center my-4 ">
+<p class=""><span className="fs-5 d-block">ECONOMY</span><span className="fs-3 d-block">$9.90</span><span className="text-secondary">BILLING PER MONTH</span> </p>
+    </div>
+    <div className="my-2 cardsbottomline">
+    </div>
+<div className="text-center">
+  <p className="my-3 fw-semibold">Bandwidth: <span className="text-danger">1GB</span></p>
+  <p className="my-3 fw-semibold">Onlinespace: <span className="text-danger">50MB</span></p>
+  <p className="my-3 fw-semibold">Support: <span className="text-danger">No</span></p>
+  <p className="my-3 fw-semibold"><span className="text-danger">1</span> Domain</p>
+  <p className="my-3 fw-semibold"><span className="text-danger">No</span> Hidden Fees</p>
+  <button className="btn btn-danger my-5 text-white">
+    JOIN NOW
+  </button>
+</div>
+  </div>
+  <div className="col-md-4  border border-dark bordersahdow">
+    <div className="text-center my-4 ">
+<p class=""><span className="fs-5 d-block">ECONOMY</span><span className="fs-3 d-block">$9.90</span><span className="text-secondary">BILLING PER MONTH</span> </p>
+    </div>
+    <div className="my-2 cardsbottomline">
+    </div>
+<div className="text-center">
+  <p className="my-3 fw-semibold">Bandwidth: <span className="text-danger">1GB</span></p>
+  <p className="my-3 fw-semibold">Onlinespace: <span className="text-danger">50MB</span></p>
+  <p className="my-3 fw-semibold">Support: <span className="text-danger">No</span></p>
+  <p className="my-3 fw-semibold"><span className="text-danger">1</span> Domain</p>
+  <p className="my-3 fw-semibold"><span className="text-danger">No</span> Hidden Fees</p>
+  <button className="btn btn-danger my-5 text-white">
+    JOIN NOW
+  </button>
+</div>
+  </div>
+  <div className="col-md-4  border border-dark bordersahdow">
+    <div className="text-center my-4 ">
+<p class=""><span className="fs-5 d-block">ECONOMY</span><span className="fs-3 d-block">$9.90</span><span className="text-secondary">BILLING PER MONTH</span> </p>
+    </div>
+    <div className="my-2 cardsbottomline">
+    </div>
+<div className="text-center">
+  <p className="my-3 fw-semibold">Bandwidth: <span className="text-danger">1GB</span></p>
+  <p className="my-3 fw-semibold">Onlinespace: <span className="text-danger">50MB</span></p>
+  <p className="my-3 fw-semibold">Support: <span className="text-danger">No</span></p>
+  <p className="my-3 fw-semibold"><span className="text-danger">1</span> Domain</p>
+  <p className="my-3 fw-semibold"><span className="text-danger">No</span> Hidden Fees</p>
+  <button className="btn btn-danger my-5 text-white">
+    JOIN NOW
+  </button>
+</div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+</div>
+
+  );
+}
+
+export default App;
